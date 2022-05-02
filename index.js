82ba6157fbcf6d4b71d0c3b7c8ec43cde2b6d9eb
Aftab Sidhu <Aftab180@gmail.com>
2022-05-02 09:38:59 -0400

    commit tree 2b07cb71c15063e7272431ba754edb68e738569e

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+const page = document.getElementById('text');
+
+window.addEventListener('DOMContentLoaded', () => {
+    page.textContent = 'This is really cool!';
+})
+
+// page.addEventListener('change', () => {
+//     page.textContent = 'This is really cool!'
+// })
+
